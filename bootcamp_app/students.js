92385ef819734d4d385e7ea7db19398d34a7509b
Julie Nguyen <julie-nguyen@live.com>
2020-01-08 18:55:49 +0000

    allow for user arguments

---
+++ bootcamp_app/students.js
@@ -0,0 +1,26 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+
+const input = process.argv; 
+
+pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts
+ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE '%${input[2]}%'
+LIMIT ${input[3] || 5};
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort} cohort`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
